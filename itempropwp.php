8d12748245da919769493f0dd23fa15cafff1518
rolandinsh <rolandinsh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-07 14:19:58 +0000

    version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/itempropwp/trunk@582679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- itempropwp.php
+++ itempropwp.php
@@ -1,9 +1,9 @@
 <?php 
 /**
- * Plugin Name: itemprop WP for SEO Rich snippets
- * Plugin URI: http://simplemediacode.info/snippets/add-itemprop-image-to-all-wordpress-images/?utm_source=wordpress&utm_medium=wpplugin&utm_campaign=itempropWP&utm_content=v-1-0-itempropWP_load_widgets
+ * Plugin Name: itemprop WP for SERP/SEO Rich snippets
+ * Plugin URI: http://simplemediacode.info/snippets/add-itemprop-image-to-all-wordpress-images/?utm_source=wordpress&utm_medium=wpplugin&utm_campaign=itempropWP&utm_content=v-2-0-itempropWP_load_widgets
  * Description: Add human invisible schema.org itemprop code to images
- * Version: 1.1
+ * Version: 2.0
  * Requires at least: 3.3
  * Tested up to: 3.4.1
  * Author: Rolands Umbrovskis
@@ -12,17 +12,30 @@
  * License URI: http://simplemediacode.com/license/gpl/
  */
 
-
-
-define('SMCIPWPV','1.1'); // location general @since 1.0
-define('SMCIPWPM',dirname(__FILE__)); // location general @since 1.0
-define('SMCIPWPF','itempropwp'); // location folder @since 1.0
-define('SMCIPWPURL', plugin_dir_url(__FILE__)); // Image location @since 1.0
-define('SMCIPWPI',SMCIPWPURL.'/img'); // Image location @since 1.0
-define('SMCIPWPORG','http://wordpress.org/extend/plugins/'.SMCIPWPF); // Image location @since 1.0
-
-add_filter('post_thumbnail_html','smc_ipwp_image_itemprop',10,3 );
-function smc_ipwp_image_itemprop($html, $post_id, $post_image_id){
- $html = str_replace('src',' itemprop="image" src',$html);
- return $html;
+/*
+ * Starting itempropwp
+*/
+itempropwp::init();
+/*
+ * itempropwp class
+ * @since 2.0
+*/
+class itempropwp {
+	// Initialize
+	public function init() {
+		define('SMCIPWPV','2.0'); // location general @since 1.0
+		define('SMCIPWPM',dirname(__FILE__)); // location general @since 1.0
+		define('SMCIPWPF','itempropwp'); // location folder @since 1.0
+		define('SMCIPWPURL', plugin_dir_url(__FILE__)); // Image location @since 1.0
+		define('SMCIPWPI',SMCIPWPURL.'/img'); // Image location @since 1.0
+		define('SMCIPWPORG','http://wordpress.org/extend/plugins/'.SMCIPWPF); // Image location @since 1.0
+		define('IPWPT',__('itemprop WP for SERP/SEO Rich snippets','itempropwp')); // Name @since 1.1
+		
+		add_filter('wp_get_attachment_image_attributes', array( 'itempropwp', 'ipwp_img_attr' ), 10, 2 ); // Adding itemprop=image to thumbnails  @since 2.0
+	}
+	
+	public function ipwp_img_attr($attr) {
+		$attr['itemprop'] = 'image';
+		return $attr;
+	}
 }
