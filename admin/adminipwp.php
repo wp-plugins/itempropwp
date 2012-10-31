b750e5d3205b30f1dc0af2652327a9ff78b8fec5
rolandinsh <rolandinsh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-31 21:47:54 +0000

    add: 3.2.0 new: admin options
    git-svn-id: https://plugins.svn.wordpress.org/itempropwp/trunk@619636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/adminipwp.php
+++ admin/adminipwp.php
@@ -2,21 +2,21 @@
 /*
  * itempropwp Admin interface
  * @since 3.1.4
- * @version 1.0
+ * @version 1.1.0
 */
 add_action('admin_menu', 'smc_ipwp_admin_menu');
 
 function smc_ipwp_admin_menu() {
 	//create new top-level menu
-	add_menu_page(__('itemprop WP for SERP/SEO Rich snippets','itempropwp'), __('itemprop WP','itempropwp'), 'activate_plugins', 'smcipwp_menu', 'smcipwp_settings');
+	add_menu_page(__('itemprop WP for SERP/SEO Rich snippets','itempropwp'), __('itemprop WP','itempropwp'), 'activate_plugins', 'smcipwp_menu', 'smcipwp_settings',SMCIPWPURL.'assets/img/itempropwp-icon-small.jpg');
 	//add_submenu_page( 'smcipwp_menu', __('Help','itempropwp'), __('Help','itempropwp'), 'edit_posts', 'smcipwphelp', 'smcipwp_help');
 	register_setting( 'smcipwp-settings', 'smcipwp_maxlenght' );
 	register_setting( 'smcipwp-settings', 'smcipwp_showcommcount' );
 	register_setting( 'smcipwp-settings', 'smcipwp_datemodified' );
-
+	register_setting( 'smcipwp-settings', 'smcipwp_perpostopt' ); /* Per post options @since 1.5.0 */
 }
 
-function smcipwp_settings(){ ?><div class="wrap"><div class="icon32" id="icon-tools"><br /></div>
+function smcipwp_settings(){ ?><div class="wrap"><img src="<?php echo SMCIPWPURL;?>assets/img/itempropwp-icon-64.jpg" alt="itempropWP" style="float: left; display:inline; margin-right:10px;" />
 	<h2><?php _e('Settings');?></h2>
 	<form method="post" action="options.php">
 <?php 
@@ -26,6 +26,7 @@ settings_fields( 'smcipwp-settings' );
 $smcipwp_maxlenght = get_option('smcipwp_maxlenght');
 $smcipwp_showcommcount = get_option('smcipwp_showcommcount');
 $smcipwp_datemodified = get_option('smcipwp_datemodified');
+$smcipwp_perpostopt=  get_option('smcipwp_perpostopt'); /* Per post options @since 1.5.0 */
 
 if(!$smcipwp_maxlenght||$smcipwp_maxlenght==''){
 	$smcipwp_maxlenght = '170'; /* well, we need some value anyway */
@@ -45,6 +46,18 @@ if(!$smcipwp_maxlenght||$smcipwp_maxlenght==''){
 		<th valign="top"><?php _e('Show dateModified?','itempropwp');?></th>
 		<td valign="top"><input type="checkbox" id="smcipwp_datemodified" name="smcipwp_datemodified" <?php checked($smcipwp_datemodified,'on') ?> /></td>
 	</tr>
+<?php 
+/* 
+Per post options 
+@since 1.5.0 
+*/
+?>
+<!-- 
+	<tr>
+		<th valign="top"><?php _e('Per post options','itempropwp');?></th>
+		<td valign="top"><input type="checkbox" id="smcipwp_perpostopt" name="smcipwp_perpostopt" <?php checked($smcipwp_perpostopt,'on') ?> /></td>
+	</tr>
+-->
  </table>
     <p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes','itempropwp') ?>" /></p>
 </form>
